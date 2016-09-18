bd71d8deab1481610830c76c302edf8e157b86e3
thecookiejar <nil>
2016-09-18 20:39:35 +0800

    a simple data model

---
+++ src/app/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using app.Data;
+
+namespace app.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {   
+            return View(PokemonProvider.Instance.GetAll);
+        }
+    }
+}
--- src/app/Startup.cs
+++ src/app/Startup.cs
@@ -16,22 +16,36 @@ namespace app
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            // Logging
             loggerFactory.AddConsole();
 
+            // Developer Exception
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
+            // Perform the routing
+            app.UseMvc(m => {
+                m.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new {
+                        controller = "Home",
+                        action = "Index"
+                    });
             });
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
         }
     }
 }
