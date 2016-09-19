252c68f0fbc7e3d9e9e3c6d8f4f138fe80cf3686
thecookiejar <nil>
2016-09-19 15:57:06 +0800

    WORKING

--- src/web/wwwroot/app/components/student/student.controller.js
+++ src/web/wwwroot/app/components/student/student.controller.js
@@ -11,20 +11,19 @@
         var vm = this;
         vm.students = [];
 
-
+        // Creating 
         vm.createStudentInput = {};
         vm.createStudent = function () {
             $http.post(site.api('students'), JSON.stringify(vm.createStudentInput))
                 .then(function (response) {
-
                     // Re-load data
                     vm.students.push(response.data);
-
                     // Close dialog
                     $('#formCreateStudent').modal('toggle');
                 });
         }
-
+        
+        // Load all 
         $http.get(site.api('students'))
             .then(function (response) {
                 vm.students = response.data;
