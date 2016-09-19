fe7e6a867055bab740ffb195dfcc0397e3b52f9d
thecookiejar <nil>
2016-09-19 16:31:12 +0800

    WORKING

--- src/web/wwwroot/app/index.module.js
+++ src/web/wwwroot/app/index.module.js
@@ -4,4 +4,14 @@
         .module('app', [
             'ui.router'
         ]);
+
+    angular.module('app').run(startup);
+    startup.$inject = ['$rootScope', '$templateCache'];
+
+    // This automatically clear the cache whenever the ng-view content changes.
+    function startup($rootScope, $templateCache) {
+        $rootScope.$on('$viewContentLoaded', function () {
+            $templateCache.removeAll();
+        });
+    }
 })();
--- src/web/wwwroot/app/index.route.js
+++ src/web/wwwroot/app/index.route.js
@@ -8,32 +8,8 @@
     angular.module('app').config(routes);
     routes.$inject = ['$stateProvider', '$urlRouterProvider'];
 
-    angular.module('app').run(startup);
-    startup.$inject = ['$rootScope', '$templateCache'];
-
     function routes($stateProvider, $urlRouterProvider) {
         $urlRouterProvider.otherwise('/');
-
-        $stateProvider
-            .state('main', {
-                url: '/',
-                templateUrl: 'app/main/main.html',
-                //controller: 'Main1Controller',
-                //controllerAs: 'vm'
-            })
-            .state('student', {
-                url: '/student',
-                templateUrl: 'app/components/student/student.html',
-                controller: 'StudentController',
-                controllerAs: 'vm'
-            });
-    }
-    
-    // This automatically clear the cache whenever the ng-view content changes.
-    function startup($rootScope, $templateCache) {
-        $rootScope.$on('$viewContentLoaded', function () {
-            $templateCache.removeAll();
-        });
     }
 
 })();
