ea7c27b003eb2ee33591507236b9681d200d39b7
javantwm <noreply@munchpress.com>
2016-12-19 21:22:24 +0800

    update

--- src/webng2/Program.cs
+++ src/webng2/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 
-namespace Webng2
+namespace webng2
 {
     public class Program
     {
